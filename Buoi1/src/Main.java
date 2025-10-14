7de602658bc991cfe3579cd1558c85dd187ec9a1
Quanle-1112 <lecongquan.dtan@gmail.com>
2025-10-14 19:29:37 +0700

    Buoi1

---
+++ Buoi1/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ten cua ban: ");
+        String name = sc.nextLine();
+        System.out.println("Chao " + name);
+    }
+}
